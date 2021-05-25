6634089e641a21796be3c3fc6f7984116433385e
brodybayley <brodybayley@gmail.com>
2021-05-25 14:43:46 -0700

    Add bill pay test case for successfully sending payment

--- cypress/integration/06.bill-pay.spec.js
+++ cypress/integration/06.bill-pay.spec.js
@@ -9,7 +9,7 @@ describe("Bill Pay", () => {
     cy.get("form").contains("Log In").click();
   });
 
-  it("displays errors when payee information left blank", () => {
+  it("should display warning messages when payee information fields left blank", () => {
     //Intentionally didn't input any values into fields so error messages would show
     cy.get("div[id=leftPanel]").contains("Bill Pay").click();
     cy.get("form").submit();
@@ -24,6 +24,28 @@ describe("Bill Pay", () => {
       .and("contain", "Phone number is required.")
       .and("contain", "Account number is required.")
       .and("contain", "The amount cannot be empty.");
+
+    cy.url().should("include", "/billpay.htm");
+  });
+
+  it("should send payment when payee information entered correctly", () => {
+    cy.get("div[id=leftPanel]").contains("Bill Pay").click();
+
+    cy.get("input[name=payee\\.name]").type("New Payee");
+    cy.get("input[name=payee\\.address\\.street]").type("1234 Happy Lane");
+    cy.get("input[name=payee\\.address\\.city]").type("Portland");
+    cy.get("input[name=payee\\.address\\.state]").type("OR");
+    cy.get("input[name=payee\\.address\\.zipCode]").type("92103");
+    cy.get("input[name=payee\\.phoneNumber]").type("1-888-486-4567");
+    cy.get("input[name=payee\\.accountNumber]").type("123456789");
+    cy.get("input[name=verifyAccount]").type("123456789");
+    cy.get("input[name=amount]").type("1000.00");
+    cy.get("form").submit();
+
+    cy.get("h1[class=title]").should("contain", "Bill Payment Complete");
+    cy.get("span[id=payeeName]").should("not.have.value", "null");
+    cy.get("span[id=amount]").should("not.have.value", "null");
+    cy.get("span[id=fromAccountId]").should("not.have.value", "null");
 
     cy.url().should("include", "/billpay.htm");
   });
