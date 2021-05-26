3e09bb6b41e177374a5ff4f34e2717bb5c77b708
brodybayley <brodybayley@gmail.com>
2021-05-25 22:23:28 -0700

    Add login test case for blank password error

--- cypress/integration/02.login.spec.js
+++ cypress/integration/02.login.spec.js
@@ -1,4 +1,5 @@
 describe("login", () => {
+  //all variable values taken from cypress.env.json
   const username = Cypress.env("username");
   const password = Cypress.env("password");
   const firstName = Cypress.env("firstName");
@@ -8,7 +9,7 @@ describe("login", () => {
     cy.visit("/index.htm");
   });
 
-  it("should display errors on login", () => {
+  it("should display errors if wrong username and password", () => {
     cy.get("input[name=username]").type("wrong username");
     cy.get("input[name=password]").type("wrong password");
     cy.get("form").contains("Log In").click();
@@ -20,6 +21,17 @@ describe("login", () => {
     cy.url().should("include", "/login.htm");
   });
 
+  it("should display error if password left blank", () => {
+    cy.get("input[name=username]").type(username);
+    cy.get("form").contains("Log In").click();
+
+    cy.get("p.error")
+      .should("be.visible")
+      .and("contain", "Please enter a username and password.");
+
+    cy.url().should("include", "/login.htm");
+  });
+
   it("should redirect to /overview.htm on successful login", () => {
     //username and password pulled from cypress.env.json
     cy.get("input[name=username]").type(username);
@@ -27,7 +39,8 @@ describe("login", () => {
     cy.get("form").contains("Log In").click();
 
     //should be redirected to overview page with welcome message on left panel
-    cy.url().should("include", "/overview.htm");
     cy.get(".smallText").should("contain", `Welcome ${firstName} ${lastName}`);
+
+    cy.url().should("include", "/overview.htm");
   });
 });
