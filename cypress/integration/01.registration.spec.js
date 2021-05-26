e64e2f8736cd94d91fce659d13893674d7b84e2b
brodybayley <brodybayley@gmail.com>
2021-05-25 21:51:57 -0700

    Add registration test case to compare password and password confirm

--- cypress/integration/01.registration.spec.js
+++ cypress/integration/01.registration.spec.js
@@ -1,15 +1,24 @@
-describe("registration", () => {
+describe("client registration", () => {
   beforeEach(function () {
     cy.visit("/register.htm");
   });
 
   it("should display errors when incomplete registration", () => {
-    //Intentionally left all fields out so errors would show
+    //Didn't update any input fields so all errors would show
     cy.get("form").contains("Register").click();
 
     cy.get("span.error")
       .should("be.visible")
-      .and("contain", "Social Security Number is required.");
+      .and("contain", "First name is required.")
+      .and("contain", "Last name is required.")
+      .and("contain", "Address is required.")
+      .and("contain", "City is required.")
+      .and("contain", "State is required.")
+      .and("contain", "Zip Code is required.")
+      .and("contain", "Social Security Number is required.")
+      .and("contain", "Username is required.")
+      .and("contain", "Password is required.")
+      .and("contain", "Password confirmation is required.");
 
     cy.url().should("include", "/register.htm");
   });
@@ -40,8 +49,34 @@ describe("registration", () => {
     cy.url().should("include", "/register.htm");
   });
 
+  it("should display error when password and password confirm don't match", () => {
+    //Ensure password and confirm don't match in order for error to show
+    const username = Cypress.env("username");
+
+    cy.get("input[name=customer\\.firstName]").type("Para");
+    cy.get("input[name=customer\\.lastName]").type("Bank");
+    cy.get("input[name=customer\\.address\\.street]").type(
+      "101 E. Huntington Drive"
+    );
+    cy.get("input[name=customer\\.address\\.city]").type("Monrovia");
+    cy.get("input[name=customer\\.address\\.state]").type("CA");
+    cy.get("input[name=customer\\.address\\.zipCode]").type("91016");
+    cy.get("input[name=customer\\.phoneNumber]").type("1-888-305-0041");
+    cy.get("input[name=customer\\.ssn]").type("123456789");
+    cy.get("input[name=customer\\.username]").type(username);
+    cy.get("input[name=customer\\.password]").type("Fake Passw0rd");
+    cy.get("input[name=repeatedPassword]").type("1234Passw0rd");
+    cy.get("form").contains("Register").click();
+
+    cy.get("span.error")
+      .should("be.visible")
+      .and("contain", "Passwords did not match.");
+
+    cy.url().should("include", "/register.htm");
+  });
+
   it("should redirect to /register.htm on successful registration", () => {
-    //Generates random client ID's for usernames
+    //generates random client ID's for usernames
     const uuid = () => Cypress._.random(0, 1e6);
     const id = uuid();
     const username = id;
