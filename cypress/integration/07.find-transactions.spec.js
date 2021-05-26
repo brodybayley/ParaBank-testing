d3c1b7a5cf23b8b45743e20f16c51c612428f911
brodybayley <brodybayley@gmail.com>
2021-05-25 19:03:42 -0700

    Add find transaction error test cases for non number inputs

--- cypress/integration/07.find-transactions.spec.js
+++ cypress/integration/07.find-transactions.spec.js
@@ -10,6 +10,55 @@ describe("Find Transactions", () => {
     cy.get("div[id=leftPanel]").contains("Find Transactions").click();
   });
 
+  it("should show an error message non numbers are inputted into find by transaction ID", () => {
+    cy.get("input[id=criteria\\.transactionId]").type("2V02F");
+    cy.get("button").eq(0).contains("Find Transactions").click();
+
+    cy.get("p[class=error]").should(
+      "contain",
+      "An internal error has occurred and has been logged."
+    );
+
+    cy.url().should("include", "/findtrans.htm");
+  });
+
+  it("should show an error message if non numbers are inputted into find by date", () => {
+    cy.get("input[id=criteria\\.onDate]").type("KK-25-2021");
+    cy.get("button").eq(1).contains("Find Transactions").click();
+
+    cy.get("p[class=error]").should(
+      "contain",
+      "An internal error has occurred and has been logged."
+    );
+
+    cy.url().should("include", "/findtrans.htm");
+  });
+
+  it("should show an error message if non numbers are inputted into find by date range", () => {
+    cy.get("input[id=criteria\\.fromDate]").type("$f-15-2021");
+    cy.get("input[id=criteria\\.toDate]").type("05-TT-2021");
+    cy.get("button").eq(2).contains("Find Transactions").click();
+
+    cy.get("p[class=error]").should(
+      "contain",
+      "An internal error has occurred and has been logged."
+    );
+
+    cy.url().should("include", "/findtrans.htm");
+  });
+
+  it("should show an error message if non numbers are inputted into find by amount", () => {
+    cy.get("input[id=criteria\\.amount]").type("A!0.00");
+    cy.get("button").eq(3).contains("Find Transactions").click();
+
+    cy.get("p[class=error]").should(
+      "contain",
+      "An internal error has occurred and has been logged."
+    );
+
+    cy.url().should("include", "/findtrans.htm");
+  });
+
   it("should find a transaction by transaction ID", () => {
     //must input an active transaction id in order for test to work
     cy.get("input[id=criteria\\.transactionId]").type("21025");
