21ff4ec1897cd5d6930fc509e7585c48f415c697
brodybayley <brodybayley@gmail.com>
2021-05-25 20:22:47 -0700

    Add logout test case to successfully log client out

---
+++ cypress/integration/08.logout.spec.js
@@ -0,0 +1,20 @@
+describe("Logout", () => {
+  const username = Cypress.env("username");
+  const password = Cypress.env("password");
+
+  beforeEach(() => {
+    cy.visit("/index.htm");
+    cy.get("input[name=username]").type(username);
+    cy.get("input[name=password]").type(password);
+    cy.get("form").contains("Log In").click();
+  });
+
+  it("should log client out", () => {
+    cy.get("div[id=leftPanel]").contains("Log Out").click();
+
+    cy.get("h2").should("contain", "Customer Login");
+    cy.get("#loginPanel").should("be.visible");
+
+    cy.url().should("include", "/index.htm");
+  });
+});
